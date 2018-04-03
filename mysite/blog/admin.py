08e92ec828a573eb6fee9eec50891f374db0cdc5
Dennis Mbithi <denmbithi@gmail.com>
2018-04-04 01:11:56 +0300

    -Created admin management for blog post

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Post
+
 
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'author', 'status', 'publish', 'created']
+    list_filter = ('created', 'title', 'status')
+    search_fields = ('title', 'body')
+    prepopulated_fields = {
+        'slug': ('title',)
+    }
+    raw_id_fields = ('author',)
+    date_hierarchy = 'publish'
+    ordering = ['status', 'publish']
+
+
+admin.site.register(Post, PostAdmin)
