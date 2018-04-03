8e916dab69a48a23c3e0acd2682b102c49522bf4
Dennis Mbithi <denmbithi@gmail.com>
2018-04-04 01:40:21 +0300

    -created the views for list and detailed posts -created templates for posts

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 # Create your views here.
+
+
+def post_list(request):
+    post = Post.publish.all()
+
+    template = 'blog/post/list.html'
+    context = {'post': post}
+
+    return render(request, template, context)
+
+
+def post_details(request, year, month, day, post):
+    post = get_object_or_404(Post,
+                             slug=post,
+                             status=published,
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day,
+                            )
+
+    template = 'blog/post/detail.html'
+    context = {'post': post}
+
+    return render(request, template, context)
